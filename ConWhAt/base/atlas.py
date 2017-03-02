175572fd57f712d6387b026dd8239657a232b4e8
John Griffiths <j.davidgriffiths@gmail.com>
2017-03-02 16:42:34 -0500

    this will be a base atlas class that will be shared with volumetric and streamlinetric

---
+++ ConWhAt/base/atlas.py
@@ -0,0 +1,28 @@
+
+class Atlas(): # object):
+  """
+
+  # base class for atlas
+
+  # is inherited by volumetric and streamlinetric
+
+  # could be connectome atlas or non-connectome atlas
+
+  # just contains
+
+  # eventuall use traits for this
+
+  """
+
+  def __init__(self):
+
+
+    self.atlas_type = None   # connectome / tract
+    self.entries = None      # list of names for entries
+    
+
+
+
+
+
+ 
