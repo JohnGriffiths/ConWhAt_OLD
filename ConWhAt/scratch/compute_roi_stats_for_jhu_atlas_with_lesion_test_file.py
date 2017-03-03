c1b37a31bf8049bbdc37cd49b2f0de72b5afbc8c
John Griffiths <j.davidgriffiths@gmail.com>
2017-03-03 12:09:28 -0500

    update scratch example

--- ConWhAt/scratch/compute_roi_stats_for_jhu_atlas_with_lesion_test_file.py
+++ ConWhAt/scratch/compute_roi_stats_for_jhu_atlas_with_lesion_test_file.py
@@ -1,14 +1,17 @@
 
 import sys
-sys.path.append('/u1/work/hpc3230/ConWhAt')
+sys.path.append('/u1/work/hpc3230/ConWhAt_brainhack')
+
 
 from ConWhAt.volumetric import VolTractAtlas
 
 va = VolTractAtlas('JHU')
 
-idxs = [0]
+idxs = [0,3,5]
 
-res = va.compute_hit_stats('lesion_test_file_from_jhu.nii.gz',idxs)
+
+les = 'lesion_test_file_from_jhu.nii.gz'
+res = va.compute_hit_stats(les,idxs)
 
 
 
