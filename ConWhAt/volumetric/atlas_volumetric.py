e68bd6e2811ce399ca54d8be7fa7f5e7df3769cd
John Griffiths <j.davidgriffiths@gmail.com>
2017-03-03 12:09:45 -0500

    fixed some bugs

--- ConWhAt/volumetric/atlas_volumetric.py
+++ ConWhAt/volumetric/atlas_volumetric.py
@@ -1,9 +1,10 @@
 
 
 from utils import get_vol_atlas_info
-from ..base import Atlas,HitStats,ROIStats
+from ..base import Atlas,compare_images,ROIStats
 
-
+import nibabel as nib
+import numpy as np
 
 
 class VolAtlas(Atlas):
@@ -21,6 +22,10 @@ class VolAtlas(Atlas):
 
   """
 
+  import nibabel as nib
+  import numpy as np
+  from nilearn.image import index_img
+
   def __init__(self,atlas_name):
 
     atlas_info = get_vol_atlas_info(atlas_name)
@@ -28,27 +33,56 @@ class VolAtlas(Atlas):
 
     self.atlas_info = atlas_info
 
-  def compute_hit_stats(self,roi_file,cnxn_ids):
+  def compute_hit_stats(self,roi_file,idxs):
+
+    """
+    idxs is a list of lists of vol ids for each file id
+    
+    so something like
+
+
+    [0, [0,1,2,3,4,5,6...]  # - default for something like JHU
+    [0, [0,1,2,3,4,5,6,...],
+     1, [0,1,2,3,4,5,6,...],
+     2, [0,1,2,3,4,5,6,...]] # default for full connectome
+    [0, [0,4,6],
+     3, [2,7,9]]              # random selection 
+
+    numbers on the left are the file number
+    numbers on the right are the (4th dim) volume ids inside that file
+ 
+    """
+
+    import nibabel as nib
+    import numpy as np
+    from nilearn.image import index_img
 
     print 'computing hit stats for roi %s' % roi_file
 
     roi_img = nib.load(roi_file)
 
-    cnxn_file = va.atlas_info['files'][0]
-    cnxns_img = nib.load(cnxn_img_file)
 
-    res = []
+    file_res = []
 
-    for idx in cnxn_ids:
+    for (file_idx,vol_idxs) in idxs:
 
-      name = va.atlas_info['names'][idx]
+      cnxns_file = self.atlas_info['files'][file_idx]
+      cnxns_img = nib.load(cnxns_file)
 
-      cnxn_img = index_img(cnxn_img,idx)
-  
-      res.append(HitStats(roi_img,cnxn_img)
+      vol_res = []
+      for vol_idx in vol_idsx:
+
+        name = self.atlas_info['names'][file_idx][vol_idx]
+
+        cnxn_img = index_img(cnxns_img,vol_idx)
       
+        comp = compare_images(roi_img,cnxn_img)
+  
+        vol_res.append(comp)
+      
+      file_res.append(vol_res)
 
-   return res
+    return res
 
 
   def compute_roi_stats(self,fa_image,cnxn_ids):
